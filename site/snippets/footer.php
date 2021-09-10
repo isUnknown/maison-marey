72626200dc4768226a068784f4b945492cdf9caf
Adrien <adrien.payet@outlook.com>
2021-09-10 16:23:40 +0200

    fix: ajustements graphiques

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,28 +1,28 @@
         </div>
         <footer>
             <div class="first">
-                <p><?= $site->title() ?><br />
+                <p class="description"><?= $site->title() ?><br />
                 <?= $site->adress() ?><br />
                 <?= $site->additional() ?><br />
                 <?= $site->postalCode() ?> <?= $site->city() ?></p>
             </div>
             <div class="second">
-                <p><?= $site->email() ?></p>
+                <p class="description"><?= $site->email() ?></p>
 
-                <p>Instagram : <a href="<?= $site->instagram() ?>" rel="noopener noreferrer">@maisonmarey</a><br />
+                <p class="description">Instagram : <a href="<?= $site->instagram() ?>" rel="noopener noreferrer">@maisonmarey</a><br />
                 Facebook : <a href="<?= $site->facebook() ?>" rel="noopener noreferrer">/maisonmarey</a></p>
             </div>
             <div class="third">
-                <p>Contact</p>
+                <p class="description">Contact</p>
 
-                <p>Retours<br />
+                <p class="description">Retours<br />
                 Mentions légales</p>
             </div>
             <div class="fourth">
-                <p>© 2021 Maison Marey.<br />
+                <p class="description">© 2021 Maison Marey.<br />
                 Tous droits réservés.</p>
 
-                <p>Design : <a href="https://www.hicetnunc-studio.fr/">Hic et Nunc studio</a><br />
+                <p class="description">Design : <a href="https://www.hicetnunc-studio.fr/">Hic et Nunc studio</a><br />
                 Developpement : Adrien Payet
                 </p>                
             </div>
